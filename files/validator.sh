0ccf94743f0f20885ad3d81f0e4d476e96274bb6
Bhargava Sai Macha <mbsai29@gmail.com>
2024-10-14 15:10:41 -0400

    add geyser

--- files/validator.sh
+++ files/validator.sh
@@ -44,4 +44,5 @@ exec /mnt/solana/target/release/solana-validator \
 --known-validator Certusm1sa411sMpV9FPqU5dXAYhmmhygvxJ23S6hJ24 \
 --known-validator 9bkyxgYxRrysC1ijd6iByp9idn112CnYTw243fdH2Uvr \
 --known-validator 12ashmTiFStQ8RGUpi1BTCinJakVyDKWjRL6SWhnbxbT \
---known-validator FdaysQ2BZWUGBy8nqFgiudnrhzJp4xChQ8B4zJdc2JZB
+--known-validator FdaysQ2BZWUGBy8nqFgiudnrhzJp4xChQ8B4zJdc2JZB \
+--geyser-plugin-config /home/solana/geyser_config.json
